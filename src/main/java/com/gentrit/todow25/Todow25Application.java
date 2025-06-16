61e654595c45653ab7f1fc02cd1a9d396d7a29ac
Gentrithotii <Gentrithoti@hotmail.com>
2025-06-16 09:40:45 +0200

    Initial commit

---
+++ src/main/java/com/gentrit/todow25/Todow25Application.java
@@ -0,0 +1,13 @@
+package com.gentrit.todow25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todow25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Todow25Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/gentrit/todow25/Todow25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gentrit.todow25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Todow25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
