ea0b08d395cf03febd6eca8f8b7488775acdd0f2
Gentrithotii <Gentrithoti@hotmail.com>
2025-06-16 10:50:56 +0200

    More logic and more functions

--- src/main/java/com/gentrit/todow25/entity/Person.java
+++ src/main/java/com/gentrit/todow25/entity/Person.java
@@ -1,13 +1,20 @@
 package com.gentrit.todow25.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
 
 @Entity
 public class Person {
-    @Getter
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false, name = "person_id")
     private Long id;
@@ -17,8 +24,22 @@ public class Person {
     @Column(nullable = false)
     private String name;
 
-    @Column(unique = true, nullable = false)
     @Getter
+    @Column(unique = true, nullable = false)
     private String email;
+
+    @Getter
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "person")
+    private List<Todo> personTodos;
+
+    public void addTodo(Todo todo) {
+        todo.setPerson(this);
+        getPersonTodos().add(todo);
+    }
+
+    public void removeTodo(Todo todo) {
+        todo.setPerson(null);
+        getPersonTodos().remove(todo);
+    }
 
 }
--- src/main/java/com/gentrit/todow25/entity/Todo.java
+++ src/main/java/com/gentrit/todow25/entity/Todo.java
@@ -1,22 +1,53 @@
 package com.gentrit.todow25.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 
 @Entity
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false, name = "todo_id")
     private long id;
+
+    @Getter
+    @Setter
+    @Column(length = 80, nullable = false)
     private String title;
+
+    @Getter
+    @Setter
     private String description;
+
+    @Getter
+    @Setter
     private boolean completed;
+
+    @Getter
     private LocalDateTime createdAt;
+
+    @Getter
+    @Setter
     private LocalDateTime updatedAt;
+
+    @Getter
+    @Setter
     private LocalDateTime dueDate;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "person_id")
     private Person person;
+
+    @PrePersist
+    public void onCreation() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
